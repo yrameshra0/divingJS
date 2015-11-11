33c07cc747ade86d2d44a75939a31066f47cb6f0
Y Ramesh Rao <ramesh.rao.y@philips.com>
2015-11-11 11:29:18 +0530

    Add Method Dispatch Test Add Generic Collection Handling Test

---
+++ test/MethodDispatchTest.js
@@ -0,0 +1,41 @@
+var assert = require('chai').assert;
+
+describe("Dynamic Dispatch", function() {
+	it("Method Dispatch", function() {
+		var methods = {
+			init: function init(args) {
+				return 'initialized ...';
+			},
+			hello: function hello(args) {
+				return 'Hello ' + args;
+			},
+			goodbye: function goodbye(args) {
+				return 'Goodbye! cruel ' + args;
+			}
+		};
+
+		function greet(options) {
+			var args = [].slice.call(arguments, 0),
+				initialized = false,
+				action = 'init';
+
+			if (typeof options === 'string' &&
+				typeof methods[options] === 'function') {
+
+				action = options;
+				args.shift();
+			}
+
+			return methods[action](args);
+		}
+
+		var test1 = greet('hello', 'world'),
+			test2 = greet('goodbye', 'world'),
+			test3 = greet('something');
+
+		assert.equal(test1, 'Hello world');
+		assert.equal(test2, 'Goodbye! cruel world');
+		assert.equal(test3, 'initialized ...');
+
+	});
+});

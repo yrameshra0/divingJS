1213817916569eee9bc3ff9b23c0957f7cec332d
Y Ramesh Rao <ramesh.rao.y@philips.com>
2015-11-12 08:15:28 +0530

    Add more functional tests

--- test/FunctionalTest.js
+++ test/FunctionalTest.js
@@ -1,34 +1,34 @@
 var assert = require('chai').assert;
 
 describe('Functional Tests', function() {
-	it('Abstracting Algorithms from Datatypes', function() {
-		var shows = [{
-				name: 'Morning Show',
-				timing: '9AM',
-				ticketPrice: '300'
-			}, {
-				name: 'Afternoon Show',
-				timing: '12AM',
-				ticketPrice: '100'
-			}, {
-				name: 'Nigth Show',
-				timing: '9PM',
-				ticketPrice: '290'
-			}],
-			books = [{
-				title: 'Dev lok',
-				author: 'Dev Dutt Patnik',
-				price: '520'
-			}, {
-				title: 'Software Craftsman',
-				author: 'Robert Martin',
-				price: '500'
-			}, {
-				title: 'Modern JS Applications',
-				author: 'Eric Elliot',
-				price: '346'
-			}];
+	var shows = [{
+			name: 'Morning Show',
+			timing: '9AM',
+			ticketPrice: '300'
+		}, {
+			name: 'Afternoon Show',
+			timing: '12AM',
+			ticketPrice: '100'
+		}, {
+			name: 'Nigth Show',
+			timing: '9PM',
+			ticketPrice: '290'
+		}],
+		books = [{
+			title: 'Dev lok',
+			author: 'Dev Dutt Patnik',
+			price: '520'
+		}, {
+			title: 'Software Craftsman',
+			author: 'Robert Martin',
+			price: '500'
+		}, {
+			title: 'Modern JS Applications',
+			author: 'Eric Elliot',
+			price: '346'
+		}];
 
+	it('Abstracting Algorithms from Datatypes', function() {
 		var sortedShows = shows.sort(function(a, b) {
 				return a.ticketPrice > b.ticketPrice;
 			}),
@@ -40,4 +40,24 @@ describe('Functional Tests', function() {
 		assert.ok(sortedBooks[0].price < sortedBooks[1].price);
 
 	});
+
+	it('Tradition looping constructs', function() {
+		var i,
+			length = books.length;
+
+		for (i = 0; i < length; i++) {
+			books[i].category = 'casual';
+		}
+
+		assert.equal(books[0].category, 'casual');
+	});
+
+	it('Functiona looping constructs', function(){
+		books.forEach(function(book){
+			book.edition = 'First';
+		});
+
+		assert.equal(books[1].edition, 'First');
+	});
+
 });

7990a1b61ae4a8c52512013546f0cb9bc9858e76
Y Ramesh Rao <ramesh.rao.y@philips.com>
2015-11-24 15:23:54 +0530

    StampIt library usage, for prototypical inheritance

--- test/objects/PrototypicalInheritanceTest.js
+++ test/objects/PrototypicalInheritanceTest.js
@@ -1,8 +1,8 @@
 var assert = require('chai').assert;
 var stampit = require('stampit');
 
-describe('Prototypical Inheritance', function() {
-    it('Using StampIt', function() {
+describe('Prototypical Inheritance using StampIt', function() {
+    it('StampIt Introduction', function() {
         var testObj = stampit()
             .methods({
                 delegateMethod: function delegateMethod() {
@@ -27,7 +27,89 @@ describe('Prototypical Inheritance', function() {
         assert.equal(testObj.instanceProp, 'instance property');
         assert.ok(testObj.hasOwnProperty('instanceProp'), 'state should be instance safe');
 
-        assert.notOk(testObj.hasOwnProperty('privateProp'), 'should hide private properties');
+        assert.notOk(testObj.hasOwnProperty('getPrivateProp'), 'should hide private properties');
         assert.equal(testObj.getPrivate(), 'private property');
+    });
+
+    it('Delegate Prototype', function() {
+        var stamp = stampit().methods({
+            delegateMethod: function delegateMethod() {
+                return 'shared property';
+            }
+        });
+
+        var obj1 = stamp();
+        var obj2 = stamp();
+
+        Object.getPrototypeOf(obj1).delegateMethod = function() {
+            return 'altered';
+        };
+
+        assert.equal(obj1.delegateMethod(), 'altered');
+        assert.equal(obj2.delegateMethod(), 'altered');
+    });
+
+    it('Concatenative Inheritance', function() {
+        var person = stampit().state({
+            name: ''
+        });
+
+        var jimmy = person({
+            name: 'Jimmy Joe'
+        });
+
+        assert.equal(jimmy.name, 'Jimmy Joe');
+    });
+
+    it('Functional Inheritance', function() {
+        var person = stampit().enclose(
+            function() {
+                var firstName = '',
+                    lastName = '';
+
+                this.getName = function getName() {
+                    return firstName + ' ' + lastName;
+                };
+
+                this.setName = function setName(options) {
+                    firstName = options.firstName;
+                    lastName = options.lastName;
+                    return this;
+                };
+            }
+        );
+
+        var jimmy = person().setName({
+            firstName: 'Jimmy',
+            lastName: 'Joe'
+        });
+
+        assert.equal(jimmy.getName(), 'Jimmy Joe');
+    });
+
+    it('Composing Objects', function() {
+        var a = stampit().enclose(function() {
+            var a = 'A';
+
+            this.getA = function getA() {
+                return a;
+            };
+        });
+
+        var b = stampit().enclose(function() {
+            var a = 'B';
+
+            this.getB = function getB() {
+                return a;
+            };
+        });
+
+        var c = stampit().compose(a, b);
+
+        var foo = c();
+
+        assert.equal(foo.getA(), 'A');
+        assert.equal(foo.getB(), 'B');
+
     });
 });

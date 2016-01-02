a4413ab51428c3d9a6383ab6a3978f0e77a5a272
Y Ramesh Rao <ramesh.rao.y@philips.com>
2016-01-02 19:26:48 +0530

    Error handling Parameter mapping through regex Small nitty gritty changes here and there

--- nodeexpresseg/index.js
+++ nodeexpresseg/index.js
@@ -4,14 +4,52 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 44444;
 
-app.get('/', function(req, res) {
-    res.set('Content-Type', 'text/plain');
 
-    res.end('Hello World from Express \n');
+// Adding some data to request object that other middleware and routes can use.
+app.use(function(request, response, next) {
+    request.foo = 'bar';
+    next();
+});
+
+app.get('/', function(request, response) {
+    response.set('Content-Type', 'text/plain');
+
+    response.end('Hello World from Express \n');
+});
+
+app.get(/(crazy|coder)/, function(request, response) {
+    var param = request.params[0]; // Whitelisted Parameter
+    response.send('Hello ' + param);
+});
+
+app.get('/fooit', function(request, response) {
+    var param = request.params[0]; // Whitelisted Parameter
+    response.send(request.foo);
+});
+
+app.get('/desperate', function(request, response, next) {
+    try {
+        var param = request.bar; // This will thorw an exception
+        param.something();
+
+        response.send(request.foo);
+    } catch (error) {
+        return next(error);
+    }
 });
 
 app.listen(port, function() {
     console.log('Listening on port ==> ' + port);
 });
 
+
+// Error Handlers for catching un seen consequences
+app.use(function(error, request, response, next) {
+    console.log(error);
+
+    // Sending out error to callee clients.
+    response.status(500).send('Your request was not handled successfully :(');
+
+
+});
 module.exports = app;
--- nodeexpresseg/test/nodeexpresstest.js
+++ nodeexpresseg/test/nodeexpresstest.js
@@ -4,9 +4,41 @@ var app = require('../index');
 
 describe('Express Tests', function() {
 
-    it('Hello World', function(done) {
+    it('Retrieve Index Page', function(done) {
         request(app)
             .get('/')
             .expect(200, 'Hello World from Express \n', done);
+    });
+
+    it('Retrieve Whitelisted Page (Regex Test - 1)', function(done) {
+        request(app)
+            .get('/crazy')
+            .expect(200, 'Hello crazy', done);
+    });
+
+    it('Retrieve Whitelisted Page (Regex Test - 2)', function(done) {
+        request(app)
+            .get('/coder')
+            .expect(200, 'Hello coder', done);
+
+    });
+
+    it('Retrieve Whitelisted Page (Regex Test - 3)', function(done) {
+        request(app)
+            .get('/crazilycoding')
+            .expect(404, done);
+
+    });
+
+    it('Middleware processing and forwarding', function(done) {
+        request(app)
+            .get('/fooit')
+            .expect(200, 'bar', done);
+    });
+
+    it('Error reporting to callee apps', function(done) {
+        request(app)
+            .get('/desperate')
+            .expect(500, 'Your request was not handled successfully :(', done);
     });
 });

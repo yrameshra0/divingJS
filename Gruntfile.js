69522410c64307d912e37f35b6ee7cbd828c872e
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-03-11 05:54:39 +0530

    Grunt file modification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,53 @@
 module.exports = function(grunt) {
-    // Do grunt-related things in here
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
+  // Do grunt-related things in here
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
 
-    grunt.initConfig({
-        // Configure mochaTest Task
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'spec',
-                    captureFile: 'results.txt', //optionally capture the reported output
-                    quiet: false, //optionally supress output to standard out (defaults to false)
-                    clearRequireCache: false, //optionally clear the require cache before running tests (defaults to false)
-                },
-                src: ['test/**/*.js', '!test/qunitTests/**/*.js', './nodeexpresseg/test/**/*.js', './user-authorization/test/**/*.js']
-            }
-        },
-        //Configuring Coverage Reporter over mocha
-        coverage: {
-            options: {
-                reporter: 'html-cov',
-                //use the quiet flag to supress the mocha console output
-                quiet: true,
-                //specify a destination file to capture the mocha output (the quiet option does not supress this)
-                captureFile: 'coverage.html'
-            },
-            src: ['test/**/*.js']
-        },
-        // Configure jshint Task
-        jshint: {
-            files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js', './guestlisteg/src/**/*.js', './guestlisteg/test/**/*.js',
-                './guestlistbackboneeg/src/**/*.js', './guestlistbackboneeg/src/**/*.js', '!test/qunitTests/bower_components/**/*.js',
-                'nodeexpresseg/*.js', 'nodeexpresseg/test/**/*.js', 'user-authorization/**/*.js'
-            ],
-            options: {
+  grunt.initConfig({
 
-            }
+    // Configure mochaTest Task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', //optionally capture the reported output
+          quiet: false, //optionally supress output to standard out (defaults to false)
+          clearRequireCache: false, //optionally clear the require cache before running tests (defaults to false)
         },
-        // Configuring Browserify
-        browserify: {
-            dist: {
-                files: {
-                    './guestlisteg/public/app.js': ['./guestlisteg/src/*.js'],
-                    './guestlistbackboneeg/public/app.js': ['./guestlistbackboneeg/src/*.js']
-                }
-            },
-            options: {
-                banner: '//*************** ENJOYING JAVASCRIPT DIVING ***************'
-            }
-        },
-        // QUnit
-        qunit: {
-            all: ['./guestlisteg/test/*.html',
-                './test/qunitTests/*.html'
-            ]
+        src: ['test/**/*.js', '!test/qunitTests/**/*.js', './nodeexpresseg/test/**/*.js', './user-authorization/test/**/*.js']
+      }
+    },
+
+    // Configure jshint Task
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js', './guestlisteg/src/**/*.js', './guestlisteg/test/**/*.js', './guestlistbackboneeg/src/**/*.js', './guestlistbackboneeg/src/**/*.js', '!test/qunitTests/bower_components/**/*.js', 'nodeexpresseg/*.js', 'nodeexpresseg/test/**/*.js', 'user-authorization/**/*.js'],
+      options: {
+
+      }
+    },
+
+    // Configuring Browserify
+    browserify: {
+      dist: {
+        files: {
+          './guestlisteg/public/app.js': ['./guestlisteg/src/*.js'],
+          './guestlistbackboneeg/public/app.js': ['./guestlistbackboneeg/src/*.js']
         }
+      },
+      options: {
+        banner: '//*************** ENJOYING JAVASCRIPT DIVING ***************'
+      }
+    },
 
-    });
+    // QUnit
+    qunit: {
+      all: ['./guestlisteg/test/*.html', './test/qunitTests/*.html']
+    }
 
-    grunt.registerTask('default', ['jshint', 'mochaTest', 'qunit']);
-    grunt.registerTask('install', 'browserify');
+  });
+
+  grunt.registerTask('default', ['jshint', 'mochaTest', 'qunit']);
+  grunt.registerTask('install', 'browserify');
 };

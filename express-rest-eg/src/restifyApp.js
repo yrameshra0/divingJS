d1c3d4c42f3abb363d5502e70fb6fa16b1cef748
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-03-28 19:39:46 +0530

    Create and retrieving of the resource in RESTful ways.

--- express-rest-eg/src/restifyApp.js
+++ express-rest-eg/src/restifyApp.js
@@ -2,8 +2,11 @@ var express = require('express'),
   methodOverride = require('method-override'),
   bodyParser = require('body-parser'),
   errorHandler = require('express-error-handler'),
+  assign = require('lodash/object/assign'),
+  cuid = require('cuid'),
   app = express(),
-  port = 77777;
+  port = 77777,
+  albums = [];
 
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.json());
@@ -12,7 +15,49 @@ app.get('/getMethodOverride', function(request, response) {
   response.end("Successfully POST redirected to GET -- " + request.body.foo);
 });
 
-app.all('/getMethodOverride', errorHandler.httpError(405));
+app.get('/albums', function(request, response) {
+  var index = albums.map(function(album) {
+    return {
+      href: '/albums' + album.id,
+      properties: {
+        name: album.name,
+        artist: album.artist
+      }
+    };
+  });
+
+  response.send(index);
+});
+
+app.post('/albums', function(request, response) {
+  var id = cuid(),
+    album = assign({}, request.body, {
+      id: id
+    });
+
+  albums[id] = album;
+
+  response.status(201).send({
+    href: '/albums/' + id
+  });
+});
+
+app.get('/albums/:id', function(request, response, next) {
+  var id = request.params.id,
+    body = albums[id],
+    err;
+
+  if (!body) {
+    err = new Error('Album not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  response.status(200).send(body);
+});
+
+
+app.all('/albums/:id', errorHandler.httpError(405));
 
 app.listen(port, function() {
   console.log('Listening on port ==> ' + port);
--- express-rest-eg/test/restifyAppTest.js
+++ express-rest-eg/test/restifyAppTest.js
@@ -16,7 +16,51 @@ describe('RESTful Application Tests', function() {
 
   it("Method Not Allowed at place of Not Found", function(done) {
     request(app)
-      .put('/getMethodOverride')
+      .post('/albums/123')
       .expect(405, done);
+  });
+
+});
+
+describe('RESTful Album Application', function() {
+
+  var createdAlbumId = 'someId';
+  it('GET All Albums', function(done) {
+    request(app)
+      .get('/albums')
+      .expect(200, done);
+  });
+
+  it('POST Album', function(done) {
+    var album = {
+      'name': 'Fitoor',
+      'artist': 'Amit Trivedi'
+    };
+
+    request(app)
+      .post('/albums')
+      .send(album)
+      .expect(201)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        createdAlbumId = res.body;
+        console.log("CreatedAlbumId -- " + JSON.stringify(createdAlbumId));
+        done();
+      });
+
+  });
+
+  //# Maybe not a good idea to depend on the order of the execution of the test cases here
+  it('GET Created Album', function(done) {
+    request(app)
+      .get(createdAlbumId.href)
+      .expect(200, done);
+  });
+
+  it('GET Unkown Album By ID ', function(done) {
+    request(app)
+      .get("/albums/unkownAlbumId")
+      .expect(404, done);
   });
 });

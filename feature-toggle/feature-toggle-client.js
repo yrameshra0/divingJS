55200e27a9abf9e464c7a6950ed7826d25f072f4
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-04-05 20:51:01 +0530

    Window location queryparams to key-map conversion

--- feature-toggle/feature-toggle-client.js
+++ feature-toggle/feature-toggle-client.js
@@ -4,8 +4,27 @@ var union = require('mout/array/union'),
   stampit = require('stampit'),
   exportVar = {
     getParams: function getParams() {
-      return [1, 2, 3];
+      var params = {},
+        queryParams = locationSearch();
+
+      if (queryParams) {
+        var parts = queryParams.slice(1).split('&');
+        parts.forEach(function(part) {
+          var pair = part.split('=');
+          pair[0] = decodeURIComponent(pair[0]);
+          pair[1] = decodeURIComponent(pair[1]);
+          console.log(pair[0] + " -- " + pair[1]);
+          params[pair[0]] = (pair[1] !== 'undefined') ? pair[1] : true;
+        });
+      }
+
+      console.log(params);
+      return params;
+    },
+    locationSearch: function locationSearch() {
+      return window.location.search;
     }
+
   };
 
 module.exports = exportVar;
--- feature-toggle/test/feature-toggle-client-test.js
+++ feature-toggle/test/feature-toggle-client-test.js
@@ -3,8 +3,11 @@ var assert = require('chai').assert,
 
 describe('Feature Toggle Client Tests', function() {
   it('Retrives request parameters', function() {
+    Object.getPrototypeOf(featureToggle).locationSearch = function() {
+      return '?param1=1&param2=2&param3=3';
+    };
     var params = featureToggle.getParams();
 
-    assert.equal(params.length, 3);
+    assert.equal(Object.keys(params).length, 3);
   });
 });

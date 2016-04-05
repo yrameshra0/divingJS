053b638eb2f0a1f7fb74967846265fa93d49bb1d
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-04-05 21:35:16 +0530

    Feature parameter list splitting

--- feature-toggle/feature-toggle-client.js
+++ feature-toggle/feature-toggle-client.js
@@ -2,27 +2,32 @@ var union = require('mout/array/union'),
   contains = require('mout/array/contains'),
   EventEmitter = require('events').EventEmitter,
   stampit = require('stampit'),
-  exportVar = {
-    getParams: function getParams() {
-      var params = {},
-        queryParams = locationSearch();
+  getParams = function getParams() {
+    var params = {},
+      queryParams = locationSearch();
 
-      if (queryParams) {
-        var parts = queryParams.slice(1).split('&');
-        parts.forEach(function(part) {
-          var pair = part.split('=');
-          pair[0] = decodeURIComponent(pair[0]);
-          pair[1] = decodeURIComponent(pair[1]);
-          params[pair[0]] = (pair[1] !== 'undefined') ? pair[1] : true;
-        });
-      }
-
-      return params;
-    },
-    locationSearch: function locationSearch() {
-      return window.location.search;
+    if (queryParams) {
+      var parts = queryParams.slice(1).split('&');
+      parts.forEach(function(part) {
+        var pair = part.split('=');
+        pair[0] = decodeURIComponent(pair[0]);
+        pair[1] = decodeURIComponent(pair[1]);
+        params[pair[0]] = (pair[1] !== 'undefined') ? pair[1] : true;
+      });
     }
 
+    return params;
+  },
+  getParamFeatures = function getParamFeatures() {
+    var features = getParams().ft;
+    return features ? features.split(',') : 'undefined';
+  },
+  exportVar = {
+    getParams: getParams,
+    locationSearch: function locationSearch() {
+      return window.location.search;
+    },
+    getParamFeatures: getParamFeatures,
   };
 
 module.exports = exportVar;
--- feature-toggle/test/feature-toggle-client-test.js
+++ feature-toggle/test/feature-toggle-client-test.js
@@ -9,10 +9,16 @@ describe('Feature Toggle Client Tests', function() {
     };
   };
 
-  it('Retrives request parameters', function() {
+  it('Query parameters to key-map', function() {
     mockFeatureLocationSearch('?param1=1&param2=2&param3=3');
 
     var params = featureToggle.getParams();
     assert.equal(Object.keys(params).length, 3);
+  });
+
+  it('Query parameters to feature list', function() {
+    mockFeatureLocationSearch('?param1=1&param2=2&param3=3&ft=ft1,ft2');
+
+    assert.equal(featureToggle.getParamFeatures().length, 2);
   });
 });
